e6d08c9a31b7cd5ce7f8f449c01256fbea30c934
Nikhilesh <venkatasubr2@wisc.edu>
2019-12-04 21:12:46 -0600

    Added methods for GraphADT interface

--- Graph.java
+++ Graph.java
@@ -1,5 +1,42 @@
+import java.util.Set;
 
-public class Graph {
+public class Graph implements GraphADT<Person> {
+
+  @Override
+  public boolean addEdge(Person person1, Person person2) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean removeEdge(Person person1, Person person2) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean addNode(Person person) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Set<Person> getNeighbours(Person person) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Person getNode(String name) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Set<Person> getAllNodes() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 //My name is Nikhilesh and I am dumb asf
     
     //My name is Kashin and I spill food
--- GraphADT.java
+++ GraphADT.java
@@ -1,4 +1,22 @@
+import java.util.Set;
 
-public interface GraphADT {
+/**
+ * Interface that the graph class implements 
+ *
+ */
+public interface GraphADT <K> {
 
+  public boolean addEdge(K person1, K person2);
+  
+  public boolean removeEdge(K person1, K person2);
+  
+  public boolean addNode(K person);
+  
+  public Set<K> getNeighbours(K person);
+  
+  public K getNode(String name);
+  
+  public Set<K> getAllNodes();
+  
+  
 }
