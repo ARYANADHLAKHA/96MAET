cc347b26728f706614e5d7145a3e42a35c4d6712
Nikhilesh <venkatasubr2@wisc.edu>
2019-12-05 18:58:36 -0600

    Made changes to the Person Class

--- GraphADT.java
+++ GraphADT.java
@@ -1,24 +1,22 @@
 import java.util.Set;
 
 /**
- * Interface that the graph class implements 
+ * Interface that the graph class implements
  *
  */
-public interface GraphADT <K> {
+public interface GraphADT<K> {
 
   public boolean addEdge(K person1, K person2);
-  
+
   public boolean removeEdge(K person1, K person2);
-  
+
   public boolean addNode(K person);
-  
+
   public Set<K> getNeighbours(K person);
-  
+
   public K getNode(String name);
-  
+
   public Set<K> getAllNodes();
-  
-  
 
 
 
