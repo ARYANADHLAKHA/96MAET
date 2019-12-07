28eef4bd59557741867ff6ae7ce2551c4106ab78
Nikhilesh <venkatasubr2@wisc.edu>
2019-12-07 17:20:05 -0600

    added the methods from kashin

--- SocialNetwork.java
+++ SocialNetwork.java
@@ -5,48 +5,151 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class SocialNetwork implements SocialNetworkADT<Person, Graph> {
-  
-  private Graph newSocialNetwork;
+
+  private Graph graphObject;
 
   public SocialNetwork() {
-  this.newSocialNetwork = new Graph();
+    this.graphObject = new Graph();
   }
-  
+
   @Override
   public boolean addFriends(String name1, String name2) {
-    // TODO Auto-generated method stub
+
+
+
+    Person personOne = graphObject.getNode(name1);
+
+    Person personTwo = graphObject.getNode(name2);
+
+
+
+    if (graphObject.addEdge(personOne, personTwo) == true) {
+
+      return true;
+
+    }
+
+
+
     return false;
+
   }
 
+
+
   @Override
+
   public boolean removeFriends(String name1, String name2) {
-    // TODO Auto-generated method stub
+
+
+
+    Person personOne = graphObject.getNode(name1);
+
+    Person personTwo = graphObject.getNode(name2);
+
+
+
+    if (graphObject.removeEdge(personOne, personTwo) == true) {
+
+      return true;
+
+    }
+
+
+
     return false;
+
   }
 
+
+
   @Override
+
   public boolean addUser(String name) {
-    // TODO Auto-generated method stub
+
+    Person user = new Person(name);
+
+
+
+    if (graphObject.addNode(user) == true) {
+
+      return true;
+
+    }
+
+
+
     return false;
+
   }
 
+
+
   @Override
+
   public boolean removeUser(String name) {
-    // TODO Auto-generated method stub
+
+    Person user = new Person(name);
+
+
+
+    if (graphObject.removeNode(user) == true) {
+
+      return true;
+
+    }
+
+
+
     return false;
+
   }
 
+
+
   @Override
+
   public Set<Person> getFriends(String name) {
-    // TODO Auto-generated method stub
-    return null;
+
+
+
+    Person personOne = new Person(name);
+
+
+
+    return graphObject.getNeighbours(personOne);
+
+
+
   }
 
+
+
+  // do exception handling for this
+
   @Override
+
   public Set<Person> getMutualFriends(String name1, String name2) {
-    // TODO Auto-generated method stub
-    return null;
+
+
+
+    Person personOne = new Person(name1);
+
+    Person personTwo = new Person(name2);
+
+
+
+    Set<Person> personOneFriends = graphObject.getNeighbours(personOne);
+
+    Set<Person> personTwoFriends = graphObject.getNeighbours(personTwo);
+
+    personOneFriends.retainAll(personTwoFriends);
+
+    return personOneFriends;
+
   }
+
+
 
   @Override
   public List<Person> getShortestPath(String name1, String name2) {
@@ -65,8 +168,11 @@ public class SocialNetwork implements SocialNetworkADT<Person, Graph> {
 
     try {
       Scanner sc = new Scanner(toLoad);
-      while(sc.hasNextLine()) {
+      while (sc.hasNextLine()) {
         String newLine = sc.nextLine();
+        char command = (newLine.trim()).charAt(0);
+        
+
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
